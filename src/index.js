634197f7226164c4b3afe7235cd9144626e0e1dd
LinCenYing <lincenying@qq.com>
2026-06-22 14:41:57 +0800

    chore: update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import clone from "clone";
 import uslug from "uslug";
-import Token from "markdown-it/lib/token";
+import Token from "markdown-it/lib/token.mjs";
 
 const TOC = "@[toc]";
 const TOC_RE = /^@\[toc\]/im;
